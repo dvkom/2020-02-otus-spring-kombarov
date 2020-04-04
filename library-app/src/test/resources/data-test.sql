144897079950a524473b2dcb53f8356843eab845
dvkom <d.v.kombarov@gmail.com>
2020-04-04 14:23:28 +0700

    Перешел с mysql на h2

--- library-app/src/test/resources/data-test.sql
+++ library-app/src/test/resources/data-test.sql
@@ -1,6 +1,15 @@
-insert into author values (1, 'Isaac Asimov', 'USA', '1920-04-06');
-insert into author values (2, 'Jack London', 'USA', '1876-01-12');
-insert into genre values (1, 'Science fiction', 'Description');
-insert into genre values (2, 'Novel', 'Description');
-insert into book values (1, 'The End of Eternity', '325', 1, 1);
-insert into book values (2, 'The Sea-Wolf', '425', 2, 2);
+insert into author
+    (id, `name`, country, birth_date)
+values
+    (1, 'Isaac Asimov', 'USA', '1920-04-06'),
+    (2, 'Jack London', 'USA', '1876-01-12');
+insert into genre
+    (id, `name`, description)
+values
+    (1, 'Science fiction', 'Description'),
+    (2, 'Novel', 'Description');
+insert into book
+    (id, title, page_count, author_id, genre_id)
+values
+    (1, 'The End of Eternity', '325', 1, 1),
+    (2, 'The Sea-Wolf', '425', 2, 2);
