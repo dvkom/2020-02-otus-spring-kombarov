b0ffef9cfc8a88b75a67255a2e3132af4e7e3731
dvkom <d.v.kombarov@gmail.com>
2020-07-27 23:39:28 +0700

    lesson 18 done

--- lesson-18/library-app-back/src/main/java/ru/dvkombarov/app/Main.java
+++ lesson-18/library-app-back/src/main/java/ru/dvkombarov/app/Main.java
@@ -2,10 +2,12 @@ package ru.dvkombarov.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableCircuitBreaker
 public class Main {
 
     public static void main(String[] args) {
